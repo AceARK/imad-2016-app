af041c2c281b81c01492b4c1a23fb94861449f4b
AceARK <akhilark1088@gmail.com>
2016-09-23 22:38:53 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 //changing value of innerHTML of 'main-text'
 
-var element = document.getElementByID('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML = ('New value using code console');
