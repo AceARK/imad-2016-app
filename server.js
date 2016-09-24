98c98187afee944e290bdb2812f1e89ec4b3f9aa
AceARK <akhilark1088@gmail.com>
2016-09-23 21:17:22 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,88 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles ={
+    var articleOne  : {
+        
+    title: 'Article One of Ace Ark',
+    heading: 'Article One',
+    date: 'September 23, 2016',
+    content: `      <p>
+                    This is the first para of Article One. This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.
+                </p>
+                <p>
+                    This is the second para of Article One. This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.
+                </p>
+                <p>
+                    This is the third para of Article One. This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.This is the first para of Article One.
+                </p>
+                  <ol>
+                    <li>
+                        List item 1
+                    </li>
+                    <li>
+                         List item 2
+                    </li>
+                    <li>
+                        List item 3
+                    </li>
+                </ol>
+                
+            `
+    },
+    var articleTwo : {},
+    var article Three
+},
+
+    
+}
+
+function createTemplate(data){
+    title = data.title;
+    heading = data.heading;
+    date = data.date;
+    content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title> ${title} </title>
+            <meta name="viewport" content="width=height-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                     <a href="/"> Home </a> 
+                     <hr>
+                </div>
+                 
+                <div>
+                    <h3> ${heading} </h3>
+                </div>
+                
+                <div> 
+                    ${date}
+                </div>
+             
+                <div class="content">
+                   ${content}
+                </div>
+        
+             </div> 
+        </body>
+        
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req,res){
