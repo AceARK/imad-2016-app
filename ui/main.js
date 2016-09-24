877aec13c08fa4ab50fd0b042b197ff6ffffdbaa
AceARK <akhilark1088@gmail.com>
2016-09-23 23:07:56 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = ('New value using code console');
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+    var interval = setInterval(moveRight, 100);
+}
