77be00f677e845e6a536d4099c887a97d998ae6a
AceARK <akhilark1088@gmail.com>
2016-09-25 14:30:05 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,19 @@ button.onclick = function() {
     request.send(null);
 };
 
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('Submit');
+submit.onclick = function(){
+    //function to make a request to server and send name
+    
+    //capture name in array and send a list
+    var names = [name1, name2, name3, name4];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+     list = list + '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('nameList');
+    ul.innerHTML = list;
+};
