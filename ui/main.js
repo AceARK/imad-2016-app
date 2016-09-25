88059b0827100f1a3045524f97f659d46087597a
AceARK <akhilark1088@gmail.com>
2016-09-24 23:58:51 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,23 +3,22 @@ var button = document.getElementById('counter');
 counter = 0;
 //Create a request object
 button.onclick = function() {
-   ` var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     //Capture response and store in variable
     request.onreadystatechange = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             //Take some action
             if(request.status == 200){
-                var counter = request.responseText;`
+                var counter = request.responseText;
                 //Render variable in span
-                counter = counter + 1;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-           // }
-       // }
-  //  };// Not yet done
+            }
+        }
+    };// Not yet done
 
     //Make a request
-   ` request.open('GET','http://aceark.imad.hasura-app.io/counter',true);
-    request.send(null);`
+    request.open('GET','http://aceark.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
 
