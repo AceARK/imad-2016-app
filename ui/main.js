cdcb9c12a9cb2a4a830d788b53b76adc32d5d1d0
AceARK <akhilark1088@gmail.com>
2016-09-26 23:25:29 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,6 +47,6 @@ submit.onclick = function(){
     
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET','http://aceark.imad.hasura-app.io/submit?name=',true);
+    request.open('GET','http://aceark.imad.hasura-app.io/submit?name='+name,true);
     request.send(null);
 };
