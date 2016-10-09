7acb6d26e73dcd56c59a548737df14d8b24d6e40
AceARK <akhilark1088@gmail.com>
2016-10-08 20:45:56 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,10 +119,12 @@ app.get('/counter', function(req,res){
 
 //articleName == article-one
 //articles[articleName] == [] content object for article one
+/*
 app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
+*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
