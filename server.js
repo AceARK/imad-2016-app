057b2cf20841b1a7aabd2a3ee8bb6d333f2e019f
Akhila <akhilark1088@gmail.com>
2016-11-02 14:52:50 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.get('/sign_up_page', function (req, res) {
 
 app.get('/display', function (req, res) {
     res.sendFile(path.join(_dirname, 'ui', 'display.html'));
-}
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
