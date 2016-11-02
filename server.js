59e33bffd4f79217e520594ef80504850ab3f244
Akhila <akhilark1088@gmail.com>
2016-11-02 15:44:52 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -229,14 +229,14 @@ app.get('/display', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'display.html'));
 });
 */
-/*
+
 //animalType == type-one
 //animalsView[animalType] == [] content object for type-one
 app.get('/:animalType', function(req,res){
     var animalType = req.params.articleName;
     res.send(createAnimalViewTemplate(animalsView[animalType]));
 });
-*/
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
